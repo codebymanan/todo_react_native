db14257d5ab28603341a91435aabb3773eb6a391
Abdul Manan <39721062+vonix-ceo@users.noreply.github.com>
2021-04-17 17:46:14 +0500

    first

--- App.js
+++ App.js
@@ -5,8 +5,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taksWrapper}>
+        <Text style={styles.sectionTitle}>Today's Taks</Text>
+        <View>
+        </View>
+      </View>
     </View>
   );
 }
@@ -14,8 +17,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#ffff',
   },
+  taksWrapper: {
+
+  },
+sectionTitle: {
+
+},
+items: {
+
+},
 });
