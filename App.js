c05fadade27f76e2c4500e48c938f0bb595e9ba6
Abdul Manan <39721062+vonix-ceo@users.noreply.github.com>
2021-04-29 04:14:06 +0500

    Update App.js

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
 import {StatusBar} from 'expo-status-bar';
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import Task from "./components/Task";
 
 export default function App() {
     return (
         <View style={styles.container}>
             <View style={styles.tasksWrapper}>
                 <Text style={styles.sectionTitle}>Today's Tasks</Text>
-                <View>
+                <View style={styles.items}>
+                    <Task />
                 </View>
             </View>
         </View>
